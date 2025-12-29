9f30d6bff1cdcc50231378dd38361e06d536a0e4
harilexm <umer.abdullah9891@gmail.com>
2025-12-29 21:00:49 +0500

    model & encoder are loaded

--- deploy.py
+++ deploy.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 
 print("Current working directory:", os.getcwd())
 
+with open('iris_best_model.pkl', 'rb') as best_model: 
+    model = pickle.load(best_model)
+with open('lable_encoder.pkl', 'rb') as encoder_file:
+    encoder = pickle.load(encoder_file)
+
 
         
