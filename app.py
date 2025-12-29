78b0cb64bd2f38b7791546c85d34393bb5ee163b
harilexm <umer.abdullah9891@gmail.com>
2025-12-29 21:29:51 +0500

    error catching block added

--- app.py
+++ app.py
@@ -19,17 +19,21 @@ def index():
     probabilities = None
 
     if request.method == "POST":
-        features = [float(request.form["f1"]), float(request.form["f2"]), request.form(["f3"]), float(request.form["f4"])]
-        X = np.array(features).shape(1, -1)
+        try:
+            features = [float(request.form["f1"]), float(request.form["f2"]), request.form(["f3"]), float(request.form["f4"])]
+            X = np.array(features).shape(1, -1)
 
-        probs = model.predict_proba(X)[0]
-        class_index = np.argmax(probs)
+            probs = model.predict_proba(X)[0]
+            class_index = np.argmax(probs)
 
-        prediction = encoder.inverse_transform([class_index])[0]
-        confidence = round(probs[class_index] * 100, 2)
-        probabilities = dict(zip(encoder.classes_, (np.round(probs * 100, 2))))
+            prediction = encoder.inverse_transform([class_index])[0]
+            confidence = round(probs[class_index] * 100, 2)
+            probabilities = dict(zip(encoder.classes_, (probs * 100).round(2)))
+        except Exception as e:
+            print("prediction error:", e)
+
         
-        return render_template("index.html", prediction = prediction, confidence = confidence, probabilities = probabilities)
+    return render_template("index.html", prediction = prediction, confidence = confidence, probabilities = probabilities)
     
 if __name__ == "__main__":
     app.run(debug = True)
