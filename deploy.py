2d9e0c251cac735ac43c39b96b6e5da25e88bd46
harilexm <umer.abdullah9891@gmail.com>
2025-12-29 20:58:52 +0500

    Libraries are imported, creating Flask

--- deploy.py
+++ deploy.py
@@ -6,32 +6,5 @@ app = Flask(__name__)
 
 print("Current working directory:", os.getcwd())
 
-with open('iris_best_model.pkl', 'rb') as model_file: 
-    model = pickle.load(model_file)
-with open('scaler.pkl', 'rb') as scaler_file:
-    scaler = pickle.load(scaler_file)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    # user input data
-    sepal_length = float(request.form['sepal_length'])
-    sepal_width = float(request.form['sepal_width'])
-    petal_length = float(request.form['petal_length'])
-    petal_width = float(request.form['petal_width'])
-    
-    features = [[sepal_length, sepal_width, petal_length, petal_width]] # feature array
-    features = scaler.transform(features)                               # Preprocess
-    prediction = model.predict(features)                                # Make prediction
-    probabilities = model.predict_proba(features)                       # Get probabilities
-    
-    # Get the probability of the predicted class
-    confidence = round(max(probabilities[0]) * 100, 2)
-    
-    return render_template('result.html', prediction=prediction[0], accuracy=f"{confidence}%")
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+        
